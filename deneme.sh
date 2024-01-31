b51e10726046c58fdc581adda23a43bfd8afc860
MLKisRolling <94867695+alpkayra5@users.noreply.github.com>
2024-01-31 19:02:46 +0300

    Add files via upload

---
+++ deneme.sh
@@ -0,0 +1,15 @@
+echo --------------------------------
+echo - chatwithKayra                  -
+echo - made by Discord/ hewalsafakkac -
+echo --------------------------------
+echo Merhaba, isminiz nedir ?
+read name
+echo Tanistigima memmun oldum $name benimde adim kayra. Kac Yasindasin ?
+read yas
+echo Demek $yas yasindasin. Hoslandigin seylerden 3 tanesini sayar misin ?
+read hos1
+read hos2
+read hos3
+echo guzelmis demekki  $hos1 $hos2 $hos3  gibi seylerden hoslaniyorsun. En sevdigin yemek nedir ?
+read yemek
+echo Harika ! bende $yemek seviyorum.
